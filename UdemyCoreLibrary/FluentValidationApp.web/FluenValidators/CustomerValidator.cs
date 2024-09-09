3cdb569de3facc40b0a7d8e0e15c07c37bddf463
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-09-09 22:46:14 +0300

    CustomerValidator class ı oluşturuldu ve kurallar yazıldı

---
+++ UdemyCoreLibrary/FluentValidationApp.web/FluenValidators/CustomerValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using FluentValidationApp.web.Models;
+
+namespace FluentValidationApp.web.FluenValidators
+{
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(x => x.Name)
+    .NotEmpty()
+    .WithMessage("Name field cannot be empty.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("Email field cannot be empty.")
+                .EmailAddress()
+                .WithMessage("Please enter a valid email address.");
+
+            RuleFor(x => x.Age)
+                .NotEmpty()
+                .WithMessage("Age field cannot be empty.")
+                .InclusiveBetween(18, 60)
+                .WithMessage("Age must be between 18 and 60.");
+
+        }
+
+
+    }
+}
