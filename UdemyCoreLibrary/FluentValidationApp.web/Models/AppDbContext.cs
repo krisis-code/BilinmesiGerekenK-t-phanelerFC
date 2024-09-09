c7ef7a77c647a6693683b39b17aab616c3173751
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-09-09 22:36:00 +0300

    db oluşturuldu ve entity üzeirnden customer controller oluşturuldu

--- UdemyCoreLibrary/FluentValidationApp.web/Models/AppDbContext.cs
+++ UdemyCoreLibrary/FluentValidationApp.web/Models/AppDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace FluentValidationApp.web.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FluentValidationApp.web.Models
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+      : base(options)
+        {
+        }
+        public DbSet<Customer> Customers { get; set; }
     }
+    
 }
