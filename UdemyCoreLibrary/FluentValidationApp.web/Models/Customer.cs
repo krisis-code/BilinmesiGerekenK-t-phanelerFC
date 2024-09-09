c3c818a4a61da3e902394094c3f995ad7061487c
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-09-09 22:18:33 +0300

    Customer entity eklendi

---
+++ UdemyCoreLibrary/FluentValidationApp.web/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace FluentValidationApp.web.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public int Age { get; set; }
+    }
+}
